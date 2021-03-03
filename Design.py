01f0a30db8c6b0b9214e816bef8aab62b4194c33
hossainzarif <57715518+hossainzarif@users.noreply.github.com>
2021-03-03 22:48:31 +0600

    Added main files

--- Design.py
+++ Design.py
@@ -1,1 +1,57 @@
-import QtDesigner
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'ui_lyricshFuVln.ui'
+##
+## Created by: Qt User Interface Compiler version 5.14.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import (QCoreApplication, QMetaObject, QObject, QPoint,
+    QRect, QSize, QUrl, Qt)
+from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
+    QFontDatabase, QIcon, QLinearGradient, QPalette, QPainter, QPixmap,
+    QRadialGradient)
+from PySide2.QtWidgets import *
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(880, 600)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.vboxLayout = QVBoxLayout(self.centralwidget)
+        self.vboxLayout.setSpacing(0)
+        self.vboxLayout.setObjectName(u"vboxLayout")
+        self.vboxLayout.setContentsMargins(10, 10, 10, 10)
+        self.frame = QFrame(self.centralwidget)
+        self.frame.setObjectName(u"frame")
+        self.frame.setEnabled(True)
+        self.frame.setStyleSheet(u"background-color: rgba(163, 163, 163, 150);\n"
+"border-radius:7px;\n"
+"")
+        self.frame.setFrameShape(QFrame.NoFrame)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.pushButton = QPushButton(self.frame)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(380, 260, 75, 23))
+        self.pushButton.setAutoFillBackground(False)
+
+        self.vboxLayout.addWidget(self.frame)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+    # retranslateUi
+   
+
---
+++ ui_functions.py
@@ -0,0 +1,45 @@
+################################################################################
+##
+## BY: WANDERSON M.PIMENTA
+## PROJECT MADE WITH: Qt Designer and PySide2
+## V: 1.0.0
+##
+################################################################################
+
+## ==> GUI FILE
+from main_2 import *
+
+## ==> GLOBALS
+
+GLOBAL_STATE = 0
+
+class UIFunctions(MainWindow):
+
+
+    def uiDefinitions(self):
+
+        # REMOVE TITLE BAR
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+
+        # SET DROPSHADOW WINDOW
+        self.shadow = QGraphicsDropShadowEffect(self)
+        self.shadow.setBlurRadius(20)
+        self.shadow.setXOffset(0)
+        self.shadow.setYOffset(0)
+        self.shadow.setColor(QColor(0, 0, 0, 100))
+
+        # APPLY DROPSHADOW TO FRAME
+        self.ui.frame.setGraphicsEffect(self.shadow)
+
+        # MAXIMIZE / RESTORE
+
+        # MINIMIZE
+   
+
+     
+
+
+    ## RETURN STATUS IF WINDOWS IS MAXIMIZE OR RESTAURED
+    def returnStatus():
+        return GLOBAL_STATE
