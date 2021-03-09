57e452adfab05db7f4d5602f15914447c2e3d04f
hossainzarif <57715518+hossainzarif@users.noreply.github.com>
2021-03-09 16:37:56 +0600

    uri working

--- Spotify_lyrics.py
+++ Spotify_lyrics.py
@@ -5,3 +5,9 @@ import requests
 from Config.config import USERNAME, SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI
 from bs4 import BeautifulSoup
 
+
+scope = 'user-read-currently-playing'
+
+token = spotipy.util.prompt_for_user_token(
+    USERNAME,scope,client_id= SPOTIPY_CLIENT_ID,client_secret= SPOTIPY_CLIENT_SECRET,redirect_uri= SPOTIPY_REDIRECT_URI
+)
