05858f8a93fa2a856b333e528d5ced5440c77c46
hossainzarif <57715518+hossainzarif@users.noreply.github.com>
2021-03-09 14:57:48 +0600

    initialized

---
+++ Spotify_lyrics.py
@@ -0,0 +1,7 @@
+import sys
+import spotipy
+import spotipy.util as util
+import requests
+from Config.config import USERNAME, SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI
+from bs4 import BeautifulSoup
+
